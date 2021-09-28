ac5da204f6280d96241bee92c0b62bf9801a0154
Baldwin <Baldwincepeda@gmail.com>
2021-09-28 15:23:22 -0400

    testing

--- Vectors.cpp
+++ Vectors.cpp
@@ -1,7 +1,24 @@
 /*
 Author: Baldwin cepeda
-Description: Interview and general practice programming problems.
-contact info: Baldwincepeda@gmail.com 
+Description:This file was made for practicing C++ and to study for interviews
+contact info:
+-Baldwincepeda@gmail.com 
+-www.baldwincepeda.com
 */
 
 
+#include <iostream>
+#include <string>
+#include <iterator>
+
+using std::cout; using std::cin;
+using std::endl; using std::string;
+
+int main(){
+    string s1 = "This string will be printed";
+    cout << s1;
+    cout << 23;
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
